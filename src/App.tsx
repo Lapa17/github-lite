fc7d4df015d8a03a107a61e549c615837137ec0a
Pavel Laparevich <laparevichpavel@gmail.com>
2022-05-06 23:15:34 +0300

    began of requests to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, KeyboardEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { EmptyStatePage } from './pages/empty/EmptyStatePage';
 import { MainPage } from './pages/main/MainPage';
 import { StartPage } from './pages/start/StartPage';
+import { gitAPI, UserResponseType } from './api/gitAPI';
 
 function App() {
+  const [value, setValue] = useState('')
+  const [state, setState] = useState<UserResponseType | null>(null)
+
+  const onEnterClick = (e:KeyboardEvent<HTMLInputElement>) =>{
+    if (e.key === 'Enter') {
+      gitAPI.getUser(value).then(res => {
+        setState(res.data)
+      })
+    }
+  }
+
   return (
     <div className="App">
       logo
-      <input type="text" />
+      <input type="text"  onChange={(e)=> setValue(e.currentTarget.value)} onKeyPress={onEnterClick} />
+      {/* <button onClick={onEnterClick}>Search</button> */}
+      {state && state.html_url}
       <Routes>
       <Route path="/" element={<StartPage />} />
       <Route path="/main" element={<MainPage />} />
--- src/api/gitAPI.ts
+++ src/api/gitAPI.ts
@@ -2,16 +2,30 @@ import axios from 'axios'
 
 const instance = axios.create({
     baseURL: 'https://api.github.com',
-    withCredentials: true,
 })
 
 
 export const gitAPI = {
-    getUser(user:string) {
-        return instance.get(`/users/${user}`);
+    getUser(user: string) {
+        return instance.get<UserResponseType>(`/users/${user}`);
     },
-    getUserRepos(user:string){
+    getUserRepos(user: string) {
         return instance.get(`/users/${user}/repos`)
     }
 }
 
+export type UserResponseType = {
+    avatar_url: string
+    company: null
+    email: null
+    followers: number
+    following: number
+    html_url: string
+    id: number
+    location: null
+    login: string
+    name: null
+    node_id: string
+    public_gists: number
+    public_repos: number
+}
